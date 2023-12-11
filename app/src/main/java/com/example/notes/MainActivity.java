1a7f2ca7745ffb9617f9595090214272658312f4
hristinamineeva27@gmail.com <hristinamineeva27@gmail.com>
2023-12-11 22:45:43 +0500

    с

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.notes;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,8 +12,10 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import com.example.notes.Adapter.NotesListAdapter;
@@ -22,14 +25,17 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
 
     RecyclerView recyclerView;
     FloatingActionButton fab_add;
     NotesListAdapter notesListAdapter;
     RoomDB database;
     List<Notes> notes = new ArrayList<>();
+    SearchView searchView_home;
+    Notes selectedNote;
 
     @SuppressLint("NotifyDataSetChanged")
     @Override
@@ -49,18 +55,29 @@ public class MainActivity extends AppCompatActivity {
         final NotesClickListener notesClickListener = new NotesClickListener() {
             @Override
             public void onClick(Notes notes) {
-
+                Intent intent = new Intent(MainActivity.this, NotesTakerActivity.class);
+                intent.putExtra("old_note", notes);
+                startActivityForResult(intent, 102);
             }
 
             @Override
             public void onLongClick(Notes notes, CardView cardView) {
-
+                selectedNote = new Notes();
+                selectedNote = notes;
+                showPopUp(cardView);
             }
         };
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL));
         notesListAdapter = new NotesListAdapter(MainActivity.this, notes, notesClickListener);
         recyclerView.setAdapter(notesListAdapter);
+    }
+
+    private void showPopUp(CardView cardView) {
+        PopupMenu popupMenu = new PopupMenu(this, cardView);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.show();
     }
 
     @Override
@@ -71,6 +88,8 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler_home);
         fab_add = findViewById(R.id.fab_add);
 
+        searchView_home = findViewById(R.id.searchView_home);
+
         database = RoomDB.getInstance(this);
         notes = database.mainDAO().getAll();
 
@@ -80,5 +99,56 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, NotesTakerActivity.class);
             startActivityForResult(intent, 101);
         });
+
+        searchView_home.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filter (newText);
+                return false;
+            }
+        });
+    }
+
+    private void filter(String newText) {
+        List<Notes> filteredList = new ArrayList<>();
+        for(Notes singleNote : notes){
+            if(singleNote.getTitle().toLowerCase().contains(newText.toLowerCase())
+            ||singleNote.getNotes().toLowerCase().contains(newText.toLowerCase())){
+                filteredList.add(singleNote);
+            }
+        }
+        notesListAdapter.filterList(filteredList);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.pin:
+                if(selectedNote.isPinned()){
+                    database.mainDAO().pin(selectedNote.getID(),false);
+                    Toast.makeText(MainActivity.this, "Unpinned", Toast.LENGTH_SHORT).show();
+                } else{
+                    database.mainDAO().pin(selectedNote.getID(),true);
+                    Toast.makeText(MainActivity.this, "Pinned", Toast.LENGTH_SHORT).show();
+                }
+                notes.clear();
+                notes.addAll(database.mainDAO().getAll());
+                notesListAdapter.notifyDataSetChanged();
+                return true;
+
+            case R.id.delete:
+                database.mainDAO().delete(selectedNote);
+                notes.remove(selectedNote);
+                notesListAdapter.notifyDataSetChanged();
+                Toast.makeText(MainActivity.this, "Note removed", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
     }
 }
--- app/src/main/java/com/example/notes/NotesTakerActivity.java
+++ app/src/main/java/com/example/notes/NotesTakerActivity.java
@@ -20,6 +20,7 @@ public class NotesTakerActivity extends AppCompatActivity {
     EditText editText_title, editText_notes;
     ImageView imageView_save;
     Notes notes;
+    boolean isOldNote = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +30,15 @@ public class NotesTakerActivity extends AppCompatActivity {
         editText_notes = findViewById(R.id.editText_notes);
 
         imageView_save = findViewById(R.id.imageView_save);
+        notes = new Notes();
+        try {
+            notes = (Notes) getIntent().getSerializableExtra("old_note");
+            editText_title.setText(notes.getTitle());
+            editText_notes.setText(notes.getNotes());
+            isOldNote = true;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         imageView_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,7 +51,9 @@ public class NotesTakerActivity extends AppCompatActivity {
                 }
                 SimpleDateFormat formatter = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss");
                 Date date = new Date();
-                notes = new Notes();
+                if(!isOldNote) {
+                    notes = new Notes();
+                }
                 notes.setTitle(title);
                 notes.setNotes(description);
                 notes.setDate(formatter.format(date));
